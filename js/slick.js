35c9204791aee656172b301cb5c8628fb809efde
Aneta <aneta.kak@op.pl>
2023-06-19 18:08:23 +0200

    removed gallery

--- js/slick.js
+++ js/slick.js
@@ -2,16 +2,16 @@ $(document).ready(function () {
     $('.gallery').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
-        autoplay: true,
+        autoplay: false,
         autoplaySpeed: 3000,
         mobileFirst: true,
-        prevArrow: `<i class="slick-prev fa-solid fa-circle-arrow-left"></i>`,
-        nextArrow: `<i class="slick-next fa-solid fa-circle-arrow-right"></i>`,
+        arrows: false,
         responsive: [{
             breakpoint: 700,
             settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
+                arrows: true,
+                prevArrow: `<i class="slick-prev fa-solid fa-circle-arrow-left"></i>`,
+                nextArrow: `<i class="slick-next fa-solid fa-circle-arrow-right"></i>`
             }
         }],
 
